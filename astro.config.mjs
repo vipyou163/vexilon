81345f4f1076b90b091239e99e1a484741201b09
vipyou163 <user@example.com>
2026-07-21 21:43:29 +0800

    config: Pages URL base + repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://your-username.github.io',
-  base: '/',
+  site: 'https://vipyou163.github.io',
+  base: '/vexilon',
   build: {
     assets: '_assets',
   },
